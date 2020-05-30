a822230c38325e635c73d6d5c83ebc723a93f857
jacemica <jmica@uci.edu>
2020-05-30 10:25:05 -0700

    Made test data arrays binary

--- model.py
+++ model.py
@@ -10,17 +10,26 @@ data = keras.datasets.mnist
 x_train = keras.utils.normalize(x_train, axis=1)
 x_test = keras.utils.normalize(x_test, axis=1)
 
-model = keras.models.Sequential()
-model.add(keras.layers.Flatten())
-model.add(keras.layers.Dense(128, activation=tf.nn.relu))
-model.add(keras.layers.Dense(128, activation=tf.nn.relu))
-model.add(keras.layers.Dense(10, activation=tf.nn.softmax))
+for train in range(len(x_train)):
+    for row in range(28):
+        for x in range(28):
+            if x_train[train][row][x] != 0:
+                x_train[train][row][x] = 1
 
-model.compile(optimizer='adam',
-            loss='sparse_categorical_crossentropy',
-            metrics=['accuracy'])
-
-model.fit(x_train, y_train, epochs=3)
-
+print(x_train[0])
 plt.imshow(x_train[0])
 plt.show()
+
+# model = keras.models.Sequential()
+# model.add(keras.layers.Flatten())
+# model.add(keras.layers.Dense(128, activation=tf.nn.relu))
+# model.add(keras.layers.Dense(128, activation=tf.nn.relu))
+# model.add(keras.layers.Dense(10, activation=tf.nn.softmax))
+
+# model.compile(optimizer='adam',
+#             loss='sparse_categorical_crossentropy',
+#             metrics=['accuracy'])
+
+# model.fit(x_train, y_train, epochs=3)
+
+
