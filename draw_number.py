b06ef9ed5a3b0cd5f970f511b424ea4a9cf1d688
jacemica <jmica@uci.edu>
2020-05-31 06:39:45 -0700

    implemented prediction text

--- draw_number.py
+++ draw_number.py
@@ -9,19 +9,16 @@ from numpy import asarray
 from matplotlib import image, pyplot
 
 size = 28
-
 data = keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = data.load_data()
-x_train = keras.utils.normalize(x_train, axis=1)
 x_test = keras.utils.normalize(x_test, axis=1)
-print(x_test[0].shape)
 
 def draw(event):
     color='black'
     x1, y1 = (event.x-1), (event.y-1)
     x2, y2 = (event.x+1), (event.y+1)
     c.create_rectangle(x1,y1,x2,y2, fill=color, width=10)
-    blank.rectangle([x1-4,y1-4,x2+4,y2+4], fill=color, width=10)
+    blank.rectangle([x1-3,y1-3,x2+3,y2+3], fill=color, width=10)
 
 def end(event):
     filename = "myNum.jpg"
@@ -33,7 +30,7 @@ def end(event):
     img = img.reshape(28, 28)
     img = img.astype('float32')
     img = img/255.0
-
+    
     for i in range(len(img)):
         for j in range(len(img[i])):
             if img[i][j] == 1:
@@ -41,31 +38,26 @@ def end(event):
             else:
                 img[i][j] = 1
 
-    print(img)
-    # print("-------------")
-    # print(x_test[0])
-
     x_test[0] = img
     predictModel(x_test)
 
 def predictModel(data):
     model = tf.keras.models.load_model('numbers.model')
     prediction = model.predict(data)
-    print(np.argmax(prediction[0]))
-    print(data.shape)
+    p = np.argmax(prediction[0])
+    print(p)
+    pd = "I predict: " + str(p)
     pyplot.imshow(data[0])
+    pyplot.suptitle(pd, fontsize=14)
+    pyplot.text(0, 1, pd)
     pyplot.show()
 
 if __name__ == "__main__":
-    # myArray = [[]] * size
-    # for i in myArray:
-    #     i.append(0)
-
     root = Tk()
-    c = Canvas(root, height=280, width=280, bg='white')
+    c = Canvas(root, height=200, width=200, bg='white')
     c.pack()
 
-    myImage = Image.new("RGB", (280, 280), 'white')
+    myImage = Image.new("RGB", (200, 200), 'white')
     blank = ImageDraw.Draw(myImage)
 
     root.bind('<Return>', end)
